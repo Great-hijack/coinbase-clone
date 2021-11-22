d69383f5ae9927cc72c47f2ec3d1e615a9169152
Zhen <g.hijack333@gmail.com>
2021-11-22 11:50:05 +0800

    fixed UI in USD Dollar in Assets

--- src/components/AssetsListItem.tsx
+++ src/components/AssetsListItem.tsx
@@ -4,7 +4,6 @@ import Colors from '../constants/Colors';
 import {getLocaleCurrencyString} from '../utils';
 import DolloarImg from '../../assets/dollar.png';
 
-
 interface AssetsListItemProps {
   id: number;
   name: string;
@@ -19,17 +18,14 @@ const AssetsListItem: FC<AssetsListItemProps> = ({id, name, symbol, price, balan
   return (
     <TouchableHighlight style={styles.listItem} underlayColor="#FBFAFB">
       <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <Image
-          style={styles.logo}
-          source = {id == 0 ? DolloarImg : {uri: `https://s2.coinmarketcap.com/static/img/coins/64x64/${id}.png`}}
-        />
+        <Image style={styles.logo} source={id == 0 ? DolloarImg : {uri: `https://s2.coinmarketcap.com/static/img/coins/64x64/${id}.png`}} />
         <View style={{flex: 1, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
           <View>
             <Text style={styles.nameText}>{name}</Text>
           </View>
           <View style={{alignItems: 'flex-end'}}>
             <Text style={{}}>${getLocaleCurrencyString(balanceAsUSD.toFixed(2))}</Text>
-            <Text style={{}}>{id == 0 ? '' : `${price} ${symbol}`}</Text>
+            <Text style={id == 0 ? {display: 'none'} : {display: 'flex'}}>{`${price} ${symbol}`}</Text>
           </View>
         </View>
       </View>
