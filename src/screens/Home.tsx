641c5dc4064b4525006835720aa7e719e2a04c8d
Zhen <g.hijack333@gmail.com>
2021-12-02 01:53:26 +0800

    done ui part in home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,5 +1,5 @@
 import React, {useCallback, useEffect, useRef, useState} from 'react';
-import {StyleSheet, View, Text, Animated} from 'react-native';
+import {StyleSheet, View, Text, Animated, Image} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {HomeStackParamList} from '../navigation/AppNavigator';
 import {SafeAreaView} from 'react-native-safe-area-context';
@@ -7,6 +7,8 @@ import {Ionicons} from '@expo/vector-icons';
 import {ScrollView} from 'react-native-gesture-handler';
 
 import CBButton from '../components/CBButton';
+import MoverItem from '../components/MoverItem';
+import VisaImg from '../../assets/visa.png';
 
 type HomeScreenNavigationProp = StackNavigationProp<HomeStackParamList, 'HomeScreen'>;
 
@@ -28,13 +30,21 @@ const Home = ({navigation}: Props) => {
   };
   const totalAnimValue = useRef(new Animated.Value(1)).current;
 
+  useEffect(() => {
+    Animated.timing(totalAnimValue, {
+      toValue: isShowTotal ? 1 : 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start();
+  }, [isShowTotal]);
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <View>
           <Ionicons name="ios-menu-outline" size={30} color={'#4F4C4F'} style={styles.menuIcon} />
         </View>
-        <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+        <View style={styles.headerPriceParent}>
           <Animated.Text style={{opacity: totalAnimValue}}>
             <Text style={styles.titleTotal}>{`$2222`}</Text>
           </Animated.Text>
@@ -43,7 +53,7 @@ const Home = ({navigation}: Props) => {
         <View style={styles.headerRightContainer}>
           <View style={styles.headerGiftParent}>
             <Ionicons name="gift-outline" size={24} color="#0053FF" />
-            <Text style={{color: '#0053FF'}}>Get $ 10</Text>
+            <Text style={{color: '#0053FF', display: isShowTotal ? 'none' : 'flex'}}>Get $ 10</Text>
           </View>
           <View>
             <Ionicons name="ios-notifications-outline" size={25} color={'#4F4C4F'} style={styles.bellIcon} />
@@ -77,6 +87,37 @@ const Home = ({navigation}: Props) => {
             </View>
           </View>
         </View>
+
+        <View style={styles.moverContainer}>
+          <Text style={styles.moverTitle}>Top movers</Text>
+          <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} pagingEnabled={false}>
+            <MoverItem />
+            <MoverItem />
+            <MoverItem />
+            <MoverItem />
+            <MoverItem />
+            <MoverItem />
+            <MoverItem />
+          </ScrollView>
+        </View>
+
+        <View style={styles.coinbaseContainer}>
+          <Text style={styles.moverTitle}>Coinbase Card</Text>
+          <View style={styles.coinbaseContentParent}>
+            <View style={styles.coinImageParent}>
+              <Image source={VisaImg} style={styles.coinImage} />
+            </View>
+            <View style={{width: '100%', height: 1, backgroundColor: 'rgba(0, 0, 0, 0.6)'}}></View>
+            <View style={styles.journalParent}>
+              <Text style={styles.journalTitle}>Join the waitlist</Text>
+              <Text style={styles.journalContent}>Get rewards when you spend crypto or US dollars with our Visa debit card</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.borrowContainer}>
+          <Text style={styles.borrowTitle}>Borrow</Text>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -93,6 +134,16 @@ const styles = StyleSheet.create({
     marginTop: 30,
     paddingBottom: 10,
     justifyContent: 'space-between',
+  },
+  headerPriceParent: {
+    position: 'absolute',
+    left: '50%',
+    marginLeft: -40,
+    top: '50%',
+    marginTop: -14,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    flex: 1,
   },
   menuIcon: {
     marginLeft: 15,
@@ -167,7 +218,7 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     width: '100%',
     paddingHorizontal: '6%',
-    marginTop: 30,
+    marginTop: 40,
   },
   watchListTitle: {
     fontWeight: 'bold',
@@ -190,6 +241,73 @@ const styles = StyleSheet.create({
     color: 'gray',
     marginTop: 5,
     marginBottom: 30,
+  },
+  moverContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    width: '100%',
+    paddingHorizontal: '6%',
+    marginTop: 30,
+  },
+  moverTitle: {
+    fontWeight: 'bold',
+    fontSize: 21,
+  },
+
+  coinbaseContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    width: '100%',
+    paddingHorizontal: '6%',
+    marginTop: 40,
+  },
+  coinbaseContentParent: {
+    marginTop: 20,
+    borderRadius: 10,
+    width: '100%',
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: 'rgba(0, 0, 0, 0.6)',
+    paddingTop: 20,
+    paddingBottom: 25,
+    flexDirection: 'column',
+  },
+  coinImageParent: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingBottom: 25,
+    paddingTop: 10,
+  },
+  coinImage: {
+    width: 80,
+    height: 120,
+  },
+  journalParent: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingHorizontal: 25,
+    paddingTop: 20,
+  },
+  journalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  journalContent: {
+    fontSize: 17,
+    color: 'gray',
+    marginTop: 10,
+  },
+
+  borrowContainer: {
+    flex: 1,
+    flexDirection: 'column',
+    width: '100%',
+    paddingHorizontal: '6%',
+    marginTop: 40,
+    marginBottom: 20,
+  },
+  borrowTitle: {
+    fontWeight: 'bold',
+    fontSize: 21,
   },
 });
 
