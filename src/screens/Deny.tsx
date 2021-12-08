9409aadaa1ea7232fa0c705d71ef2bc1db6038c7
Zhen <g.hijack333@gmail.com>
2021-12-08 22:48:57 +0800

    made style in Deny.tsx

--- src/screens/Deny.tsx
+++ src/screens/Deny.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState, useRef, useCallback} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {StyleSheet, View, Text, SafeAreaView, TextInput, Pressable} from 'react-native';
+import {StyleSheet, View, Text, SafeAreaView, TextInput, Pressable, TouchableHighlight} from 'react-native';
 
 import * as userActions from '../store/actions/user';
 import * as balanceHistoryActions from '../store/actions/balancehistory';
@@ -22,57 +22,25 @@ const Deny = () => {
       const result = await dispatch(balanceHistoryActions.fetchBalanceHistoryData(profileId));
       return result;
     };
-    onBalanceData().then(res => {});
+    onBalanceData();
   }, [handleProfileId]);
 
   return (
     <SafeAreaView style={styles.screen}>
-      <View
-        style={{
-          flexDirection: 'column',
-          flex: 1,
-          height: '100%',
-          width: '100%',
-          justifyContent: 'center',
-          alignItems: 'flex-start',
-          alignSelf: 'center',
-          paddingHorizontal: '6%',
-        }}>
+      <View style={styles.viewContainer}>
         <Text style={styles.animText}>User not exist.</Text>
 
-        <View style={{marginTop: 20, flexDirection: 'column', width: '100%'}}>
-          <Text style={{color: 'white', fontSize: 16}}>Please input profile id.</Text>
-          <TextInput
-            style={{
-              color: 'white',
-              borderRadius: 8,
-              padding: 10,
-              fontSize: 16,
-              borderColor: 'gray',
-              marginTop: 8,
-              width: '100%',
-              height: 40,
-              borderWidth: StyleSheet.hairlineWidth,
-            }}
-            onChangeText={setProfileId}></TextInput>
-          <Pressable
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'center',
-              alignContent: 'flex-start',
-              alignItems: 'center',
-              marginTop: 8,
-              borderWidth: StyleSheet.hairlineWidth,
-              borderColor: 'white',
-              borderRadius: 16,
-              width: '100%',
-              height: 40,
-            }}
+        <View style={styles.inputViewContainer}>
+          <Text style={styles.textTitle}>Please input profile id.</Text>
+          <TextInput style={styles.textInput} onChangeText={setProfileId}></TextInput>
+          <TouchableHighlight
+            activeOpacity={0.9}
+            style={styles.signBtn}
             onPress={() => {
               handleProfileId(profileId);
             }}>
-            <Text style={{color: 'white', fontSize: 20}}>Sign In</Text>
-          </Pressable>
+            <Text style={styles.btnText}>Sign In</Text>
+          </TouchableHighlight>
         </View>
       </View>
     </SafeAreaView>
@@ -84,6 +52,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     backgroundColor: '#0149FF',
+  },
+  viewContainer: {
+    flexDirection: 'column',
+    flex: 1,
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'flex-start',
+    alignSelf: 'center',
+    paddingHorizontal: '6%',
   },
   header: {
     flexDirection: 'row',
@@ -105,6 +83,42 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontFamily: 'CoinbaseDisplay-Medium',
     textAlignVertical: 'center',
+  },
+  inputViewContainer: {
+    marginTop: 20,
+    flexDirection: 'column',
+    width: '100%',
+  },
+  textTitle: {
+    color: 'white',
+    fontSize: 16,
+  },
+  textInput: {
+    color: 'white',
+    borderRadius: 8,
+    padding: 10,
+    fontSize: 16,
+    borderColor: 'gray',
+    marginTop: 8,
+    width: '100%',
+    height: 40,
+    borderWidth: StyleSheet.hairlineWidth,
+  },
+  signBtn: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'flex-start',
+    alignItems: 'center',
+    marginTop: 8,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: 'white',
+    borderRadius: 16,
+    width: '100%',
+    height: 40,
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 20,
   },
 });
 
